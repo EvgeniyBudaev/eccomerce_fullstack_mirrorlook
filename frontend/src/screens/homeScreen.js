d3c52f22de1ed7506ed8126b5fc38170cd60a123
EvgeniyBudaev <49036840+EvgeniyBudaev@users.noreply.github.com>
2021-03-28 18:56:09 +0300

    Fetching Data

--- frontend/src/screens/homeScreen.js
+++ frontend/src/screens/homeScreen.js
@@ -1,9 +1,20 @@
-import React from 'react'
-import products from '../products'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
 import Product from "../components/product"
 
 
 const HomeScreen = () => {
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        async function fetchProducts() {
+            const {data} = await axios.get('/api/products/')
+            setProducts(data)
+        }
+        fetchProducts()
+    }, [])
+
     return (
         <div>
             <h1>Latest Products</h1>
--- frontend/src/screens/productScreen.js
+++ frontend/src/screens/productScreen.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import { Link } from 'react-router-dom'
 
 import products from "../products"
 
 
 const ProductScreen = ({ match }) => {
-    const product = products.find(p => p._id === match.params.id)
+    const [product, setProduct] = useState([])
+
+    useEffect(() => {
+        async function fetchProduct() {
+            const {data} = await axios.get(`/api/products/${match.params.id}`)
+            setProduct(data)
+        }
+        fetchProduct()
+    }, [])
 
     return (
         <div>
