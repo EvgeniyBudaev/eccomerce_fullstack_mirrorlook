d23af5665881f63e95c6cd0596eb891ee1b1ea39
EvgeniyBudaev <49036840+EvgeniyBudaev@users.noreply.github.com>
2021-03-30 08:09:33 +0300

    Modeling Our Data

--- backend/base/models.py
+++ backend/base/models.py
@@ -1,3 +1,100 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Product(models.Model):
+  user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)  # При True Django сохранит пустое значение как NULL в базе данных. Значение по умолчанию – False.
+  category = models.CharField(max_length=200, null=True, blank=True, verbose_name='Категория')
+  name = models.CharField(max_length=200, null=True, blank=True, verbose_name='Наименование')
+  image = models.ImageField(null=True, blank=True)
+  description = models.TextField(null=True, blank=True, verbose_name='Описание')
+  rating = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True, verbose_name='Рейтинг')
+  numReviews = models.IntegerField(null=True, blank=True, verbose_name='Комментарии')
+  price = models.DecimalField(max_digits=8, decimal_places=2, null=True, blank=True, verbose_name='Цена')
+  count_in_stock = models.IntegerField(null=True, blank=True, default=0, verbose_name='Кол-во товара')
+  code = models.CharField(max_length=32, null=True, blank=True, verbose_name='Артикул')
+  color_frame = models.CharField(max_length=64, null=True, blank=True, verbose_name='Цвет рамы')
+  color_mirror = models.CharField(max_length=64, null=True, blank=True, verbose_name='Цвет зеркала')
+  base_mirror = models.CharField(max_length=64, null=True, blank=True, verbose_name='Цвет основы')
+  height = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True, verbose_name='Высота')
+  width = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True, verbose_name='Ширина')
+  weight = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True, verbose_name='Вес')
+  type_of_installation = models.CharField(max_length=64, null=True, blank=True, verbose_name='Тип установки')
+  type_of_mounting= models.CharField(max_length=64, null=True, blank=True, verbose_name='Тип навески')
+  heightWithoutFrame = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True, verbose_name="Высота без рамы")
+  weightWithoutFrame = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True, verbose_name="Ширина без рамы")
+  faced = models.BooleanField(default=True, null=True, blank=True, verbose_name='Фацет')
+  form = models.CharField(max_length=64, null=True, blank=True, verbose_name='Форма')
+  appointment = models.CharField(max_length=200, null=True, blank=True, verbose_name='Назначение')
+  material_mirror = models.CharField(max_length=200, null=True, blank=True, verbose_name='Материал зеркала')
+  material_frame = models.CharField(max_length=200, null=True, blank=True, verbose_name='Материал рамы')
+  country_brand = models.CharField(max_length=64, null=True, blank=True, verbose_name='Страна бренда')
+  country_manufacturer = models.CharField(max_length=64, null=True, blank=True, verbose_name='Страна производства')
+  manufacturer = models.CharField(max_length=64, null=True, blank=True, verbose_name='Производитель')
+  created_at = models.DateTimeField(auto_now_add=True)
+  _id = models.AutoField(primary_key=True, editable=False)
+
+  def __str__(self):
+    return self.name
+
+  class Meta:
+    verbose_name = 'Продукт'
+    verbose_name_plural = 'Продукты'
+    ordering = ['-created_at']  # сортировка в админке
+
+
+class Review(models.Model):
+  product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+  user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+  name = models.CharField(max_length=200, null=True, blank=True, verbose_name='Наименование')
+  rating = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True, verbose_name='Рейтинг')
+  comment = models.TextField(null=True, blank=True, verbose_name='Комментарий')
+  _id = models.AutoField(primary_key=True, editable=False)
+
+  def __str__(self):
+    return str(self.rating)
+
+
+class Order(models.Model):
+  user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+  payment_method = models.CharField(max_length=200, null=True, blank=True, verbose_name='Способ оплаты')
+  tax_price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True, verbose_name='Налог')
+  shipping_price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True, verbose_name='Цена доставки')
+  total_price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True, verbose_name='Итоговая сумма')
+  isPaid = models.BooleanField(default=False, verbose_name='Статус оплаты')
+  paidAt = models.DateTimeField(auto_now_add=False, null=True, blank=True, verbose_name='Дата оплаты')
+  is_delivered = models.BooleanField(default=False, verbose_name='Статус доставки')
+  delivered_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата доставки')
+  created_at = models.DateTimeField(auto_now_add=False, null=True, blank=True, verbose_name='Дата создания')
+  _id = models.AutoField(primary_key=True, editable=False)
+
+  def __str__(self):
+    return str(self.createdAt)
+
+
+class OrderItem(models.Model):
+  product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+  order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+  name = models.CharField(max_length=200, null=True, blank=True, verbose_name='Наименование')
+  qty = models.IntegerField(null=True, blank=True, default=0, verbose_name='Кол-во')
+  price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True, verbose_name='Цена')
+  image = models.CharField(max_length=200, null=True, blank=True, verbose_name='Фото')
+  _id = models.AutoField(primary_key=True, editable=False)
+
+  def __str__(self):
+    return str(self.name)
+
+
+class ShippingAddress(models.Model):
+  order = models.OneToOneField(Order, on_delete=models.CASCADE, null=True, blank=True)
+  address = models.CharField(max_length=200, null=True, blank=True, verbose_name='Адрес')
+  city = models.CharField(max_length=200, null=True, blank=True, verbose_name='Город')
+  postalCode = models.CharField(max_length=200, null=True, blank=True, verbose_name='Индекс')
+  country = models.CharField(max_length=200, null=True, blank=True, verbose_name='Страна')
+  shippingPrice = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True, verbose_name='Стоимость доставки')
+  _id = models.AutoField(primary_key=True, editable=False)
+
+  def __str__(self):
+    return str(self.address)
--- frontend/src/screens/productScreen.js
+++ frontend/src/screens/productScreen.js
@@ -2,8 +2,6 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
 
-import products from "../products"
-
 
 const ProductScreen = ({ match }) => {
     const [product, setProduct] = useState([])
