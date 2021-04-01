030ca2db28884b7be51b01a406affb510792275d
EvgeniyBudaev <49036840+EvgeniyBudaev@users.noreply.github.com>
2021-04-01 13:25:32 +0300

    add TS to Categories Action

--- frontend/src/redux/actions/categoriesAction.ts
+++ frontend/src/redux/actions/categoriesAction.ts
@@ -1,10 +1,13 @@
 import axios from "axios"
+import {Dispatch} from "redux"
+
 import { createAsyncAction } from 'typesafe-actions'
 
 import { LOAD_CATEGORIES_REQUEST, LOAD_CATEGORIES_FAILURE, LOAD_CATEGORIES_SUCCESS } from "../../constants/categoriesConstants"
+import {AppThunk, CategoriesTypes, GetStateType, ProductByIdThunk} from "../types";
 
 
-const fetchCategories = () => async (dispatch) => {
+const fetchCategories = ():AppThunk => async (dispatch, getState: GetStateType) => {
     try {
         dispatch({type: LOAD_CATEGORIES_REQUEST})
         const {data} = await axios.get('/api/categories/')
--- frontend/src/redux/types.ts
+++ frontend/src/redux/types.ts
@@ -1,3 +1,27 @@
+import {ThunkAction} from 'redux-thunk'
+import {Action} from 'redux'
+
+import {
+  LOAD_CATEGORIES_FAILURE,
+  LOAD_CATEGORIES_REQUEST,
+  LOAD_CATEGORIES_SUCCESS
+} from "../constants/categoriesConstants"
+import {RootStateType} from "./reducers"
+
+
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootStateType,
+  unknown,
+  Action<string>
+  >
+
+export type GetStateType = () => RootStateType
+
+export type IFailure = {
+  error: Error
+}
+
 export interface ICategory {
   id: number,
   name: string,
@@ -20,3 +44,27 @@ export type MapDispatchPropsCategoriesType = {
 }
 
 export type CategoriesPropsType = MapStatePropsCategoriesType & MapDispatchPropsCategoriesType
+
+
+export type ProductByIdThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootStateType,
+  unknown,
+  Action<CategoriesTypes>
+  >
+
+export type CategoriesTypes = LoadCategoriesRequestType | LoadCategoriesSuccessType | LoadCategoriesFailureType
+
+export type LoadCategoriesRequestType = {
+  type: typeof LOAD_CATEGORIES_REQUEST
+}
+
+export type LoadCategoriesSuccessType = {
+  type: typeof LOAD_CATEGORIES_SUCCESS,
+  data: Array<ICategory>,
+}
+
+export type LoadCategoriesFailureType = {
+  type: typeof LOAD_CATEGORIES_FAILURE,
+  error: IFailure,
+}
