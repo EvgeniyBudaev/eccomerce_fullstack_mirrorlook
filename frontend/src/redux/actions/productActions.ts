c73ec0b6caa377e54a526ea823d14771d93bd8c5
EvgeniyBudaev <49036840+EvgeniyBudaev@users.noreply.github.com>
2021-04-10 10:43:22 +0300

    remove not used comments

--- frontend/src/redux/actions/productActions.ts
+++ frontend/src/redux/actions/productActions.ts
@@ -11,11 +11,8 @@ import {ProductDetailsTypes} from "../types"
 const fetchProductDetail = (category_slug: string, product_slug: string) => async (dispatch: Dispatch<ProductDetailsTypes>) => {
     try {
         dispatch({type: LOAD_PRODUCT_DETAILS_REQUEST, product_slug })
-        // const response = await axios.get(`/api/categories/${category_slug}/${product_slug}`)
-        // console.log('response', response)
         const {data} = await axios.get(`/api/categories/${category_slug}/${product_slug}`)
-
-
+        
         dispatch({type: LOAD_PRODUCT_DETAILS_SUCCESS, payload: data, product_slug})
     } catch (error) {
         dispatch({type: LOAD_PRODUCT_DETAILS_FAILURE, payload: error.response && error.response.data.detail
