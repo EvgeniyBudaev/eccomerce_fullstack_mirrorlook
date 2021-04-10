76809e4197049cec754759c8d3ff6cd428f4a196
EvgeniyBudaev <49036840+EvgeniyBudaev@users.noreply.github.com>
2021-04-10 10:48:16 +0300

    скобки раздвинул

--- frontend/src/redux/actions/productActions.ts
+++ frontend/src/redux/actions/productActions.ts
@@ -1,18 +1,18 @@
 import axios from 'axios'
-import {Dispatch} from "redux"
+import { Dispatch } from "redux"
 
 import {
     LOAD_PRODUCT_DETAILS_FAILURE,
     LOAD_PRODUCT_DETAILS_REQUEST, LOAD_PRODUCT_DETAILS_SUCCESS,
 } from "../../constants/productConstants"
-import {ProductDetailsTypes} from "../types"
+import { ProductDetailsTypes } from "../types"
 
 
 const fetchProductDetail = (category_slug: string, product_slug: string) => async (dispatch: Dispatch<ProductDetailsTypes>) => {
     try {
         dispatch({type: LOAD_PRODUCT_DETAILS_REQUEST, product_slug })
         const {data} = await axios.get(`/api/categories/${category_slug}/${product_slug}`)
-        
+
         dispatch({type: LOAD_PRODUCT_DETAILS_SUCCESS, payload: data, product_slug})
     } catch (error) {
         dispatch({type: LOAD_PRODUCT_DETAILS_FAILURE, payload: error.response && error.response.data.detail
