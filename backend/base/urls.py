2009a2f39ae40524a518370f4c65b5b2923b5dbc
EvgeniyBudaev <budaev.e@gmail.com>
2021-04-12 12:51:19 +0300

    User Serializer

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,16 +1,15 @@
 from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView
-)
+
 from . import views
 
 
 urlpatterns = [
-  path('users/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
   path('', views.get_routes, name="routes"),
   path('categories/', views.get_categories, name="categories"),
   path('products/', views.get_products, name="products"),
   path('products/<str:pk>/', views.get_product, name="product"),
   path('categories/<slug:category_slug>/', views.get_products_by_category, name='products_by_category'),
   path('categories/<slug:category_slug>/<slug:product_slug>/', views.get_product_by_category, name='product_by_category'),
+  path('users/profile/', views.getUserProfile, name="users-profile"),
+  path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
 ]
--- backend/base/views.py
+++ backend/base/views.py
@@ -4,14 +4,32 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from rest_framework import status
-from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework import generics
+from django_filters.rest_framework import DjangoFilterBackend
+
 
 from .models import Product, Category
-from .serializers import ProductSerializer, CategorySerializer
+from .serializers import ProductSerializer, CategorySerializer, UserSerializer, UserSerializerWithToken
 
 
 # Create your views here.
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+  def validate(self, attrs):
+    data = super().validate(attrs)
+
+    serializer = UserSerializerWithToken(self.user).data
+    for k, v in serializer.items():
+      data[k] = v
+
+    return data
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+  serializer_class = MyTokenObtainPairSerializer
 
 
 @api_view(['GET'])
@@ -37,6 +55,14 @@ def get_routes(request):
   ]
 
   return Response(routes)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getUserProfile(request):
+  user = request.user
+  serializer = UserSerializer(user, many=False)
+  return Response(serializer.data)
 
 
 @api_view(['GET'])
