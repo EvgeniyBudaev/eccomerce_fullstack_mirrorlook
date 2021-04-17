f2c00602d733f76f1cc5a8f30339d5c026895642
EvgeniyBudaev <budaev.e@gmail.com>
2021-04-17 19:53:37 +0300

    fix

--- backend/base/models.py
+++ backend/base/models.py
@@ -85,7 +85,7 @@ class Order(models.Model):
   id = models.AutoField(primary_key=True, editable=False)
 
   def __str__(self):
-    return str(self.createdAt)
+    return str(self.created_at)
 
 
 class OrderItem(models.Model):
