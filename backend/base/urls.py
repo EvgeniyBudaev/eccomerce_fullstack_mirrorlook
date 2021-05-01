091d1f84142344ebecb0a1c0a5076094467acd01
EvgeniyBudaev <49036840+EvgeniyBudaev@users.noreply.github.com>
2021-05-01 09:03:45 +0300

    backend: Adminka: Get User By ID & Update User Endpoints

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -17,6 +17,9 @@ urlpatterns = [
   path('users/login/', views.MyTokenObtainPairView.as_view(),
        name='token_obtain_pair'),
   path('users/register/', views.registerUser, name='register'),
+
+  path('users/<str:pk>/', views.getUserById, name='user'),
+  path('users/update/<str:pk>/', views.updateUser, name='user-update'),
   path('users/delete/<str:pk>/', views.deleteUser, name='user-delete'),
 
   path('orders/add/', views.addOrderItems, name='orders-add'),
